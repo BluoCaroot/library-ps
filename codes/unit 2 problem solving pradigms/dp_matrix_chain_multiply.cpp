53fa2ad4feeadde36c8c0f746698b22ea1c348fa
BluoCaroot <assem.akl2004@gmail.com>
2025-01-20 00:26:31 +0200

    fix

--- codes/unit 2 problem solving pradigms/dp_matrix_chain_multiply.cpp
+++ codes/unit 2 problem solving pradigms/dp_matrix_chain_multiply.cpp
@@ -5,7 +5,7 @@ int dp[100][100];
 
 int matrix_chain_multiply(vector<int> &arr, int i, int j)
 {
-    if (i == j)
+    if (i + 1 == j)
     {
         return 0;
     }
@@ -14,9 +14,9 @@ int matrix_chain_multiply(vector<int> &arr, int i, int j)
         return dp[i][j];
     }
     dp[i][j] = INT_MAX;
-    for (int k = i; k < j; k++)
+    for (int k = i + 1; k < j; k++)
     {
-        dp[i][j] = min(dp[i][j], matrix_chain_multiply(arr, i, k) + matrix_chain_multiply(arr, k + 1, j) + arr[i - 1] * arr[k] * arr[j]);
+        dp[i][j] = min(dp[i][j], matrix_chain_multiply(arr, i, k) + matrix_chain_multiply(arr, k, j) + arr[i] * arr[k] * arr[j]);
     }
     return dp[i][j];
 }
