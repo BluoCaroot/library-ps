105131ffae76baccb5fb954c6d163459e362393c
Assem Amr Abdelfattah <114243953+BluoCaroot@users.noreply.github.com>
2025-01-26 12:41:57 +0200

    Update Euclidean_algorithm.cpp

--- codes/unit 3 Number Theory/Euclidean_algorithm.cpp
+++ codes/unit 3 Number Theory/Euclidean_algorithm.cpp
@@ -36,12 +36,3 @@ int extended_euclidean_algorithm(int a, int b, int &x, int &y)
 }
 
 
-double area(const vector<point>& fig) {
-    double res = 0;
-    for (unsigned i = 0; i < fig.size(); i++) {
-        point p = i ? fig[i - 1] : fig.back();
-        point q = fig[i];
-        res += (p.x - q.x) * (p.y + q.y);
-    }
-    return fabs(res) / 2;
-}
